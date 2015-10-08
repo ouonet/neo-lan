2ae4a8a3c1e94a90d688a17235dd2bad4ecf9e30
neo <liang.gong@gmail.com>
2015-10-09 00:05:56 +0800

    1.0.0 JS的对象继承

---
+++ index.js
@@ -0,0 +1,75 @@
+/**
+ * Created by neo on 2015/10/7.
+ */
+'use strict';
+module.exports = (function () {
+    // inline overrides
+    var ua = typeof navigator == "object" && (typeof navigator.userAgent == 'string' )
+            ? navigator.userAgent.toLowerCase() : "",
+        check = function (r) {
+            return r.test(ua);
+        },
+        isOpera = check(/opera/),
+        isIE = !isOpera && check(/msie/),
+        apply = function (o, c, defaults) {
+            // no "this" reference for friendly out of scope calls
+            if (defaults) {
+                apply(o, defaults);
+            }
+            if (o && c && typeof c == 'object') {
+                for (var p in c) {
+                    o[p] = c[p];
+                }
+            }
+            return o;
+        },
+        io = function (o) {
+            for (var m in o) {
+                this[m] = o[m];
+            }
+        },
+        override = function (origclass, overrides) {
+            if (overrides) {
+                var p = origclass.prototype;
+                apply(p, overrides);
+                if (isIE && overrides.hasOwnProperty('toString')) {
+                    p.toString = overrides.toString;
+                }
+            }
+        },
+        oc = Object.prototype.constructor;
+
+    return function (sb, sp, overrides) {
+        if (typeof sp == 'object') {
+            overrides = sp;
+            sp = sb;
+            sb = overrides.constructor != oc ? overrides.constructor : function () {
+                sp.apply(this, arguments);
+            };
+        }
+        var F = function () {
+            },
+            sbp,
+            spp = sp.prototype;
+
+        F.prototype = spp;
+        sbp = sb.prototype = new F();
+        sbp.constructor = sb;
+        sb.superclass = spp;
+        if (spp.constructor == oc) {
+            spp.constructor = sp;
+        }
+        sb.override = function (o) {
+            override(sb, o);
+        };
+        sbp.superclass = sbp.supr = (function () {
+            return spp;
+        });
+        sbp.override = io;
+        override(sb, overrides);
+        sb.extend = function (o) {
+            return extend(sb, o);
+        };
+        return sb;
+    };
+})();
---
+++ test/test1.js
@@ -0,0 +1,126 @@
+/**
+ * Created by neo on 2015/10/7.
+ */
+var extend = require('../index');
+var should = require('should');
+describe("instance of subclass should be SubClass and SuperClass", function () {
+    it('instance type', function () {
+        function Creature() {
+
+        }
+
+        function Animal() {
+            return this;
+        }
+
+        extend(Animal, Creature);
+        function Sheep() {
+            Animal.call(this);
+            return this;
+        }
+
+        extend(Sheep, Animal, {
+            name: function () {
+                return 'sheep';
+            }
+        });
+        var sheep = new Sheep();
+        sheep.should.be.an.instanceOf(Sheep);
+        sheep.should.be.an.instanceOf(Animal);
+        sheep.should.be.an.instanceOf(Creature);
+    })
+});
+
+describe("instance of subclass should have properties and methods of SuperClass", function () {
+    it('Employee inherits from Person', function () {
+        function Person() {
+            this.name = "jack";
+            this.age = 0;
+        }
+        function Employee(){
+            Person.call(this);
+            this.jobtitle="";
+        }
+        extend(Employee,Person);
+        var employee=new Employee();
+        employee.should.have.properties('name');
+        employee.name.should.be.exactly('jack');
+        employee.should.have.properties('age');
+        employee.should.have.properties('jobtitle');
+    })
+});
+
+describe("same property of different instance should have different value ", function () {
+    it('compare two instance', function () {
+        function Education(){
+            this.degree="bachelor";
+            return this;
+        }
+        function Person() {
+            this.name = "";
+            this.age = 0;
+            this.education=new Education();
+            return this;
+        }
+        function Employee(){
+            Person.call(this);
+            this.jobtitle="";
+        }
+        extend(Employee,Person);
+        var employee1=new Employee();
+        var employee2=new Employee();
+        should.notStrictEqual(employee1.education,employee2.education);
+    })
+});
+
+describe("different instance should have same function", function () {
+    it('compare two instance', function () {
+        function Education(){
+            this.degree="bachelor";
+            return this;
+        }
+        function Person() {
+            this.name = "";
+            this.age = 0;
+            this.education=new Education();
+            return this;
+        }
+        Person.prototype.sayHello=function(){
+            console.log('hi');
+        }
+        function Employee(){
+            Person.call(this);
+            this.jobtitle="";
+        }
+        extend(Employee,Person);
+        var employee1=new Employee();
+        var employee2=new Employee();
+        should.strictEqual(employee1.sayHello,employee2.sayHello);
+    })
+});
+
+describe("Methods added to SubClass will not been added to SuperClass", function () {
+    it('compare two prototype', function () {
+        function Education(){
+            this.degree="bachelor";
+            return this;
+        }
+        function Person() {
+            this.name = "";
+            this.age = 0;
+            this.education=new Education();
+            return this;
+        }
+        Person.prototype.sayHello=function(){
+            console.log('hi');
+        }
+        function Employee(){
+            Person.call(this);
+            this.jobtitle="";
+        }
+        extend(Employee,Person);
+        //var employee1=new Employee();
+        //var employee2=new Employee();
+        should.notEqual(Employee.prototype,Person.prototype);
+    })
+});
